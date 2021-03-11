3d08d36bb7109de437bb4b246483036931dbc306
BenOmerDev <ben.w.omer@gmail.com>
2021-03-11 16:42:45 -0600

    Updates to dashboard and login functions

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
 firebase.auth().onAuthStateChanged(async function(user) {
+  let db = firebase.firestore()
+  console.log(db)
+
+
+
   if (user) {
     // Signed in
     console.log('signed in')
-  } else {
+    
+    db.collection('users').doc(user.uid).set({
+      email: user.email,
+      name: user.displayName
+    })
+    document.querySelector('.subscriptions').insertAdjacentHTML('beforeend',`
+      <div class="mt-8 mb-2 mx-16 px-8 py-4 text-4xl text-green-600 font-bold text-center border-8 rounded-full border-gray-600">
+          <h1>Subscription Tracker</h1>`)
+    
+    document.querySelector('.subscriptions').insertAdjacentHTML('beforeend',`
+      <div class=" mx-8   flex text-4xl text-center">
+          <div class="w-1/3 px-2 py-4">
+              ☁
+          </div>
+          <div class="w-1/3 px-2 py-4">
+              📅
+          </div>
+          <div class="w-1/3 px-2 py-4">
+              💲
+          </div>
+      </div>`)
+
+    document.querySelector('.subscriptions').insertAdjacentHTML('beforeend', `
+      <div class="mx-8 flex">
+        <div class="w-1/3 mt-4 mb-2 px-8 text-center py-4 text-2xl text-black-600 
+        font-bold border-8 rounded-l-full border-green-600">🍕</div>
+        <div class="w-1/3 mt-4 mb-2 px-8 text-center py-4 text-2xl text-black-600 
+        font-bold border-8 border-green-600">17th</div>
+        <div class="w-1/3 mt-4 mb-2 px-8 text-center py-4 text-2xl text-black-600 
+        font-bold border-8 rounded-r-full border-green-600">$3.99</div>
+      </div>`)
+
+    // Sign out button
+    document.querySelector('.sign-in-or-sign-out').innerHTML = `
+      <div class="text-white-500">Signed in as ${user.displayName}</div>
+      <button class="text-pink-500 underline sign-out">Sign Out</button>`
+
+    // Signs out if 
+    document.querySelector('.sign-out').addEventListener('click', function(event) {
+      console.log('sign out clicked')
+      firebase.auth().signOut()
+      document.location.href = 'index.html'
+    })
+  }
+  else {
     // Signed out
     console.log('signed out')
 
