f9853762817c62d8390423f6090e65999a7a0f75
BenOmerDev <ben.w.omer@gmail.com>
2021-04-01 10:49:33 -0500

    netlify Firebase update

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -2,13 +2,12 @@ const firebase = require("firebase/app")
 require("firebase/firestore")
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVkSPZVMZPK3UBibMwBmo_2d97KlcJE3Y",
-  authDomain: "kiei-451-ec081.firebaseapp.com",
-  projectId: "kiei-451-ec081",
-  storageBucket: "kiei-451-ec081.appspot.com",
-  messagingSenderId: "306609130724",
-  appId: "1:306609130724:web:0fe13d2a2ce346f4d970f1",
-  measurementId: "G-MWYKVEHD5V"
+  apiKey: "AIzaSyDh2FBNkWVf5b0wkqDEskXsOnb6ac-Db3Y",
+  authDomain: "kiei-451-d33e6.firebaseapp.com",
+  projectId: "kiei-451-d33e6",
+  storageBucket: "kiei-451-d33e6.appspot.com",
+  messagingSenderId: "364866132437",
+  appId: "1:364866132437:web:789b432547f3aa129fc55c"
 } // replace
 
 if (!firebase.apps.length) {
